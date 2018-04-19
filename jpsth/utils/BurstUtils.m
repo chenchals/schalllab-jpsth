a5e2f0c0f0e73ffc3cb74dbe1b120924205b7172
Chenchal Subraveti <chenchals@gmail.com>
2018-04-19 15:24:03 -0500

    Added methods: alignForTrials, loadBursts, createArgParser

--- jpsth/utils/BurstUtils.m
+++ jpsth/utils/BurstUtils.m
@@ -38,7 +38,7 @@ classdef BurstUtils
             if isempty(timeWin) % use minmax of spike times for every trial
                 timeWins = cellfun(@(x) [min(x) max(x)],cellSpikeTimes,'UniformOutput',false);
                 % if there are any empty timeWins, ie the trial had NaN or
-                % zero spikes then, set those timeWins to [0 0] 
+                % zero spikes then, set those timeWins to [0 0]
                 for ii=1:size(timeWins,1)
                     if isempty(timeWins{ii})
                         timeWins{ii,:}=[0 0];
@@ -65,7 +65,8 @@ classdef BurstUtils
             %fx_t = @(fn) cellfun(@(x) x.(fn),allBursts,'UniformOutput',false);
         end
         
-        function outputArg = loadCellBursts(cellNos,fileList,blacklistedUIDs)           
+        function outputArg = loadBursts(cellNos,fileList,blacklistedUIDs)
+            outputArg = struct();
             for i = 1:numel(cellNos)
                 cellNo = cellNos(i);
                 cellUID =  num2str(cellNo,'UID_%04d');
@@ -76,7 +77,38 @@ classdef BurstUtils
                 end
             end
         end
+        
+        function outputArg = alignForTrials(burstTimes, varargin)
+            defaultArgs = {'alignTimes', [], @isnumeric,...
+                'timeWin', [], @(x) isnumeric(x) && numel(x)==2 && diff(x)>0,...
+                'trials', [] @isnumeric};
+            argParser = BurstUtils.createArgParser(defaultArgs);
+            if ~isempty(varargin)
+                argParser.parse(varargin{:});
+            end
+            args = argParser.Results;
+            % do alignTimes first
+            if numel(args.alignTimes)==1
+                outputArg = cellfun(@(b) b-args.alignTimes, burstTimes,'UniformOutput', false);
+            elseif numel(args.alignTimes) == size(burstTimes,1)
+                alignTimes = arrayfun(@(x) {x}, args.alignTimes);
+                outputArg = cellfun(@(b,t) b-t, burstTimes, alignTimes, 'UniformOutput', false);
+            else
+                error('Number of times in alignTimes must be 1 or equal to no of trials in burstTimes');
+            end
+            % Select trials, intentionally selecting AFTER alignment
+            if numel(args.trials) > 0
+                outputArg = outputArg(args.trials);
+            end
+            % output only windowed bursts
+            if ~isempty(args.timeWin)
+                outputArg = cellfun(@(x) x(x>=args.timeWin(1) && x<=args.timeWin(2)),...
+                                    outputArg, 'UniformOutput', false);
+            end
             
+        end
+        
+        
         function saveOutput(oFile, resCellArray,varargin)
             % SAVEOUTPUT Saves output of burst analysis for a single unit
             % varargin:
@@ -113,7 +145,24 @@ classdef BurstUtils
                 clearvars t temp tempvals
             end
         end
-         
+        
+    end
+    
+    methods (Static, Access=private)
+        
+        function argParser = createArgParser(varargin)
+           argParser = inputParser();
+           args = varargin{1};
+           if numel(args) == 0 || mod(numel(args),3)==1
+               error(['When creating argParser the no. of argumets must be greater than zero '...
+                   'and must be EVEN corresponding to key-value pairs, where value is the default value']);
+           end
+           for  i = 1:3:numel(args)
+               argParser.addParameter(args{i},args{i+1},args{i+2});
+           end
+           argParser.parse();
+        end
+        
     end
     
 end
