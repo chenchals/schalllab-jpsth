296ee1fb248bf5a3ff578b7258d581c4b0a8ea80
Chenchal Subraveti <chenchals@gmail.com>
2018-04-24 19:10:19 -0500

    updated for parfor, added wait for file write to complete...

--- jpsth/forThomas/exploreData.m
+++ jpsth/forThomas/exploreData.m
@@ -1,11 +1,4 @@
-% CREATEBURSTSDB Runs poissBurst on sigle units for all sessions.  The cell
-%   information/parameters for each single unit is defined in each row in
-%   the CellInfoDB.mat file. 
-% Notes: DO NOT USE DET struct for any analysis
-%   Target aliged spikes (for Da valid for MG and SAT, no DET)
-%         Time win: [-2500 3500]
-%   To align on RT:(valid for SAT only)
-%          spikeTimes - movesStruct.
+
     allDaData ='/mnt/teba/Users/Thomas/0-chenchal/Info/Darwin.mat'; % not useful
     analysisDir = '/mnt/teba/Users/Thomas/0-chenchal/BurstAnalysis/burstAlignedDBDarwin';
     monk = 'Da'; % used as suffix for the info
@@ -42,20 +35,21 @@
     cellNos = 1:nCells;
      % for each cell -> ensure spike times is a celll array of {nTrials x 1 }
      
-     parfor c = 1:nCells
-         for tt = 1:numel(useTrialTypes)            
-             fprintf('Detecting bursts for Cell #%d',c);
+    parfor c = 1:nCells
+         for tt = 1:numel(useTrialTypes) 
+             fprintf('Detecting bursts for Cell #%d and trialType %s \n',c,useTrialTypes{tt});
              trialType = useTrialTypes{tt};
-             cellInfo = CellInfoDB(tt,:);
+             cellInfo = CellInfoDB(c,:);
              sessionNo = cellInfo.SessionNo;
              % Save 1 file for each trial type
              fileName = char(join({cellInfo.UID,'session',num2str(sessionNo,'%03d'),'aligned', trialType},'_'));
-             analysisFile = fullfile(analysisDir, fileName);
+             analysisFile = fullfile(analysisDir, [fileName '.mat']);
              % Convert to colum of trials with row
              spkTimes = spikeData.(trialType){c}';
              timeWins = cellfun(@(x) minmax(x),spkTimes,'UniformOutput',false);
              oBursts = BurstUtils.detectBursts(spkTimes,timeWins);
              BurstUtils.saveOutput(analysisFile,oBursts,'cellInfo',cellInfo);
+             waitForFileOutput(analysisFile);
              fprintf('wrote file %s\n\n',analysisFile);
              % since we are using aligned timestamps need ot create a
              % logical array for isBursting
@@ -70,6 +64,12 @@
          oBursts.isBursting = BurstUtils.convert2logical(oBursts.bobT,oBursts.eobT,[-2500 3500]);
          %overwrite
          save(analysisFile, '-struct', 'oBursts');
+     end
+     
+     function waitForFileOutput(analysisFile)
+          while(~exist(analysisFile,'file'))
+              pause(0.2);
+          end
      end
 
      
