6ab0c375f85aed75d81ab800e8f429d504490bfb
Chenchal Subraveti <chenchals@gmail.com>
2018-09-25 10:38:31 -0500

    added code to extract SOB

--- jpsth/forLegendy/exploreBursts.m
+++ jpsth/forLegendy/exploreBursts.m
@@ -1,14 +1,14 @@
 
 %% List mat files created for analyzing bursts
 baseDir = '/Volumes/schalllab/Users/Chenchal/Legendy';
-signfFolders = getFullpaths([baseDir filesep 'Bursts_Signif_*']);
+signfFolders = getFullpaths([baseDir filesep 'Burst_Prob_*']);
 burstFiles = cellfun(@(x) getFullpaths([x filesep 'burstDB' filesep 'UID*']),signfFolders,'UniformOutput',false);
 burstAlignedFiles = cellfun(@(x) getFullpaths([x filesep 'burstDB' filesep 'UID*']),signfFolders,'UniformOutput',false);
 burstAlignedTimeWindowFiles = cellfun(@(x) getFullpaths([x filesep 'burstDB' filesep 'UID*']),signfFolders,'UniformOutput',false);
 %% Plot bursts over PSTH for different Surprise levels
 currGroup = burstFiles;
 si = cell(575,5);
-for gr = 3:numel(currGroup)
+for gr = 1:numel(currGroup)
     currFiles = currGroup{gr};
     for f = 1:numel(currFiles)
        sob = load(currFiles{f},'sob');
