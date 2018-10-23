f839b7a473b65cb7bae6389da452ed32486f49d2
Chenchal Subraveti <chenchals@gmail.com>
2018-10-23 15:14:43 -0500

    Updated as function

---
+++ scripts/jpsth/getCellPairsForJpsth.m
@@ -0,0 +1,9 @@
+ 
+inFile = 'Darwin_SAT_CellInfoTable';
+inRootAnalysisDir = '~/teba/local/Users/Chenchal/JPSTH/';
+rootDataDir = '/Volumes/schalllab/data';
+
+load(fullfile(inRootAnalysisDir,inFile));
+
+
+
--- scripts/jpsth/readSummaryExcel.m
+++ scripts/jpsth/readSummaryExcel.m
@@ -1,101 +1,106 @@
-rootAnalysisDir = '~/teba/local';
-rootDataDir = '/Volumes/schalllab/data';
+function [darwinCellInfos, eulerCellInfos] = readSummaryExcel()
 
-summaryFileDir = fullfile(rootAnalysisDir,'Users/Chenchal/JPSTH/');
-darwinSummaryFile = fullfile(summaryFileDir,'Darwin_SAT_colorRecode.xlsx');
-eulerSummaryFile = fullfile(summaryFileDir,'Euler_SAT_colorRecode.xlsx');
+    rootAnalysisDir = '~/teba/local';
+    rootDataDir = '/Volumes/schalllab/data';
 
-% Directory of translated Data files: .mat files
-darwinMatDir = fullfile(rootDataDir,'Darwin/SAT/Matlab');
-eulerMatDir = fullfile(rootDataDir,'Euler/SAT/Matlab');
+    summaryFileDir = fullfile(rootAnalysisDir,'Users/Chenchal/JPSTH/');
+    darwinSummaryFile = fullfile(summaryFileDir,'Darwin_SAT_colorRecode.xlsx');
+    eulerSummaryFile = fullfile(summaryFileDir,'Euler_SAT_colorRecode.xlsx');
 
-% Directory of raw Data files: .plx files
-darwinPlxDir = fullfile(rootDataDir,'Darwin/SAT/Plexon/Sorted');
-eulerPlxDir = fullfile(rootDataDir,'Euler/SAT/Plexon/Sorted');
+    % Directory of translated Data files: .mat files
+    darwinMatDir = fullfile(rootDataDir,'Darwin/SAT/Matlab');
+    eulerMatDir = fullfile(rootDataDir,'Euler/SAT/Matlab');
 
-% Darwin files
-darwin.matfiles = struct2table(dir(fullfile(darwinMatDir,'D*.mat'))); 
-darwin.plxfiles = struct2table(dir(fullfile(darwinPlxDir,'D*.plx'))); 
-% Euler files
-euler.matfiles = struct2table(dir(fullfile(eulerMatDir,'E*.mat'))); 
-euler.plxfiles = struct2table(dir(fullfile(eulerPlxDir,'E*.plx'))); 
+    % Directory of raw Data files: .plx files
+    darwinPlxDir = fullfile(rootDataDir,'Darwin/SAT/Plexon/Sorted');
+    eulerPlxDir = fullfile(rootDataDir,'Euler/SAT/Plexon/Sorted');
+
+    % Darwin files
+    darwin.matfiles = struct2table(dir(fullfile(darwinMatDir,'D*.mat')));
+    darwin.plxfiles = struct2table(dir(fullfile(darwinPlxDir,'D*.plx')));
+    % Euler files
+    euler.matfiles = struct2table(dir(fullfile(eulerMatDir,'E*.mat')));
+    euler.plxfiles = struct2table(dir(fullfile(eulerPlxDir,'E*.plx')));
+
+    darwinCellInfos = parseRawExcel(darwinSummaryFile, darwin.matfiles);
+    eulerCellInfos = parseRawExcel(eulerSummaryFile, euler.matfiles);
 
 
-
-[~,~,rawD] = xlsread(darwinSummaryFile);
-
-[~,~,rawE] = xlsread(eulerSummaryFile);
-
-% Single unit quality metric
-rawCell = rawE;
-matfiles = euler.matfiles;
-taskFileContains = {'DET','MG','SEARCH','ZAP'};
-
-% find numbers
-containsNumbers = cellfun(@isnumeric,rawCell);
-%# convert to string
-rawCell(containsNumbers) = cellfun(@num2str,rawCell(containsNumbers),'UniformOutput',false);
-
-temp = rawCell(1:4,1); % top 4 rows
-qualityMetric = struct();
-for ii = 1:size(temp,1)
-    raw = temp{ii};
-    raw_ = split(regexprep(raw,'\s*=\s*','='),'=');
-    qualityMetric(ii).raw = char(raw);
-    qualityMetric(ii).val = str2double(raw_{1});
-    qualityMetric(ii).comment = char(raw_{2});
-    if isnan(qualityMetric(ii).val)
-        % convert '< .5', was always 0.25
-        qualityMetric(ii).val = 0.25;
-    end
 end
-clearvars ii temp raw raw_;
-lastColumn = 23;
-colNames = rawCell(6,1:lastColumn);
-temp = rawCell(7:end,1:lastColumn);
-allNanRows = find(cell2mat(arrayfun(@(x) all(isnan([temp{x,:}])),(1:size(temp))','UniformOutput',false)));
-temp(allNanRows,:)=[];
-sessionNames = regexp([temp{:,1}],'[A-Z]\d+','match')';
-expr = char(join(sessionNames,'|'));
-sessionNameRows = find(cell2mat(arrayfun(@(x) max([regexp(char(temp{x,1}),expr,'start'),0]) == 1,...
-    (1:size(temp,1))','UniformOutput',false)));
-nSessions = numel(sessionNameRows);
-nRows = size(temp,1);
-cellInfos = [];
-for ii = 1:nSessions
-    if ii < nSessions
-        sessionRows = sessionNameRows(ii):sessionNameRows(ii+1)-1;
-    else
-        sessionRows = sessionNameRows(ii):nRows;
-    end
-    % Process session Information
-    tempSession = cell2table(temp(sessionRows(1),1:2),'VariableNames',{'SessionName', 'SessionNotes'});
-    tempTaskTrials = cell2table(temp(sessionRows(2:4),1:2),'VariableNames',{'TaskName', 'NTrials'});
-    tempTaskTrials = cell2table(tempTaskTrials{:,2}','VariableNames',strcat('nTrials_',tempTaskTrials{:,1})');
-    
-    tempFiles = matfiles.name(~cellfun('isempty',regexp(matfiles.name,tempSession.SessionName,'match')));
-    
-    tempTaskFiles = table();
-    for t=1:numel(taskFileContains)
-        task = taskFileContains{t};
-        if contains(lower(task),'zap')
-            f = tempFiles(contains(lower(tempFiles),'zap'));
-        else
-            f = tempFiles(contains(tempFiles,task) & ~contains(lower(tempFiles),'zap') );
-        end
-        if isempty(f)
-           tempTaskFiles.([task '_matfile']){1} = '';
-        else
-            tempTaskFiles.([task '_matfile']){1} = f{1};
+
+function [cellInfos] = parseRawExcel(excelSummaryFile, matfiles)
+    [~,~,rawCell] = xlsread(excelSummaryFile);
+    taskFileContains = {'DET','MG','SEARCH','ZAP'};
+    % find numbers
+    containsNumbers = cellfun(@isnumeric,rawCell);
+    %# convert to string
+    rawCell(containsNumbers) = cellfun(@num2str,rawCell(containsNumbers),'UniformOutput',false);
+    % Single unit quality metric
+    % Not used...
+    temp = rawCell(1:4,1); % top 4 rows
+    qualityMetric = struct();
+    for ii = 1:size(temp,1)
+        raw = temp{ii};
+        raw_ = split(regexprep(raw,'\s*=\s*','='),'=');
+        qualityMetric(ii).raw = char(raw);
+        qualityMetric(ii).val = str2double(raw_{1});
+        qualityMetric(ii).comment = char(raw_{2});
+        if isnan(qualityMetric(ii).val)
+            % convert '< .5', was always 0.25
+            qualityMetric(ii).val = 0.25;
         end
     end
-    
-    % process Unit information
-    tempUnitTable = cell2table(temp(sessionRows(2:end),3:end),'VariableNames',colNames(3:end));
-    tempUnitTable(strcmpi(tempUnitTable.NeuronNumber,'NaN'),:) = [];
-    tempUnitTable.Unit = regexprep(tempUnitTable.Unit,'^(\d[a-z])','0$1');
-    for c = 1:size(tempUnitTable,1)
-        cellInfos = [cellInfos; [tempSession tempTaskFiles  tempTaskTrials tempUnitTable(c,:)]];
+    clearvars ii temp raw raw_;
+    lastColumn = 23;
+    colNames = rawCell(6,1:lastColumn);
+    temp = rawCell(7:end,1:lastColumn);
+    allNanRows = cell2mat(arrayfun(@(x) all(isnan([temp{x,:}])),(1:size(temp))','UniformOutput',false));
+    temp(allNanRows,:)=[];
+    sessionNames = regexp([temp{:,1}],'[A-Z]\d+','match')';
+    expr = char(join(sessionNames,'|'));
+    sessionNameRows = find(cell2mat(arrayfun(@(x) max([regexp(char(temp{x,1}),expr,'start'),0]) == 1,...
+        (1:size(temp,1))','UniformOutput',false)));
+    nSessions = numel(sessionNameRows);
+    nRows = size(temp,1);
+    cellInfos = [];
+    for ii = 1:nSessions
+        if ii < nSessions
+            sessionRows = sessionNameRows(ii):sessionNameRows(ii+1)-1;
+        else
+            sessionRows = sessionNameRows(ii):nRows;
+        end
+        % Process session Information
+        tempSession = cell2table(temp(sessionRows(1),1:2),'VariableNames',{'SessionName', 'SessionNotes'});
+        tempTaskTrials = cell2table(temp(sessionRows(2:4),1:2),'VariableNames',{'TaskName', 'NTrials'});
+        tempTaskTrials = cell2table(tempTaskTrials{:,2}','VariableNames',strcat('nTrials_',tempTaskTrials{:,1})');
+
+        tempFiles = matfiles.name(~cellfun('isempty',regexp(matfiles.name,tempSession.SessionName,'match')));
+
+        tempTaskFiles = table();
+        for t=1:numel(taskFileContains)
+            task = taskFileContains{t};
+            if contains(lower(task),'zap')
+                f = tempFiles(contains(lower(tempFiles),'zap'));
+            else
+                f = tempFiles(contains(tempFiles,task) & ~contains(lower(tempFiles),'zap') );
+            end
+            if isempty(f)
+                tempTaskFiles.([task '_matfile']){1} = '';
+            else
+                tempTaskFiles.([task '_matfile']){1} = f{1};
+            end
+        end
+
+        % process Unit information
+        tempUnitTable = cell2table(temp(sessionRows(2:end),3:end),'VariableNames',colNames(3:end));
+        tempUnitTable(strcmpi(tempUnitTable.NeuronNumber,'NaN'),:) = [];
+        tempUnitTable.Unit = regexprep(tempUnitTable.Unit,'^(\d[a-z])','0$1');
+        for c = 1:size(tempUnitTable,1)
+            cellInfos = [cellInfos; [tempSession tempTaskFiles  tempTaskTrials tempUnitTable(c,:)]]; %#ok<AGROW>
+        end
     end
 end
+
+
+
 
