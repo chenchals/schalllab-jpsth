7a0e9772375d59664d266e24a2ebab45208eeabc
Chenchal Subraveti <chenchals@gmail.com>
2018-11-09 16:47:41 -0600

    updated SpikeUtils with jpsth, jeromiahJpsth, rasters, psth...

--- jpsth/utils/SpikeUtils.m
+++ jpsth/utils/SpikeUtils.m
@@ -5,14 +5,61 @@ classdef SpikeUtils
     %
     
     methods (Static, Access=public)
-        function outArg = jpsth(spikes1, spike2, alignTime, timeWin, coinsBins)
-
-        
+        function outArg = jpsth(alignedSpikesX, alignedSpikesY, timeWin, binWidth, coincidenceBins)
             
+            
+            nTrials = size(alignedSpikesX,1);
+            
+            outArg.xRasters = SpikeUtils.rasters(alignedSpikesX,timeWin);
+            outArg.yRasters = SpikeUtils.rasters(alignedSpikesY,timeWin);
+            xPsth = SpikeUtils.psth(alignedSpikesX,binWidth,timeWin);
+            yPsth = SpikeUtils.psth(alignedSpikesY,binWidth,timeWin);
+            outArg.xPsthBins = xPsth.psthBins;
+            outArg.xSpikeCounts = xPsth.spikeCounts;
+            outArg.xPsth = xPsth.psth;
+            outArg.xPsthStd = xPsth.psthStd;
+            outArg.yPsthBins = yPsth.psthBins;
+            outArg.ySpikeCounts = yPsth.spikeCounts;
+            outArg.yPsth = yPsth.psth;
+            outArg.yPsthStd = yPsth.psthStd;
+            outArg.nTrials = nTrials;
+            outArg.binWidth = binWidth;
+            outArg.coincidenceBins = coincidenceBins;
+            outArg.timeBins = {timeWin(1):binWidth:timeWin(2)};
+            
+            % Cross correlation histogram for -lag:lag bins of JPSTH
+            fx_xcorrh = @(jpsth,lagBins)...
+                [-abs(lagBins):abs(lagBins);arrayfun(@(x) mean(diag(jpsth,x)),...
+                -abs(lagBins):abs(lagBins))]';
+            % Coincidence Histogram
+            fx_coinh = @getCoincidence;
+
+            
+            % JPSTH Equations from Aertsen et al. 1989
+            % Note bins [1,1] is top-left and [n,n] is bottom-right
+            outArg.rawJpsth = (outArg.xSpikeCounts'*outArg.ySpikeCounts)/(nTrials*binWidth^2); % Eq. 3
+            predicted = outArg.xPsth' * outArg.yPsth;			           % Eq. 4
+            unnormalizedJpsth = outArg.rawJpsth - predicted;             % Eq. 5
+            normalizer = outArg.xPsthStd' * outArg.yPsthStd;                           % Eq. 7a
+            outArg.normJpsth = unnormalizedJpsth ./ normalizer;          % Eq. 9
+            outArg.normJpsth(isnan(outArg.normJpsth)) = 0;
+            % lagBins for xCorr
+            outArg.xCorrHist = fx_xcorrh(outArg.normJpsth,floor(numel(outArg.timeBins)/2));
+            % Coincidence Hist
+            outArg.coinHist = fx_coinh(outArg.normJpsth,coincidenceBins);
+        end
+        
+        function outArg = jeromiahJpsth(alignedSpikesX, alignedSpikesY, timeWin, binWidth, coincidenceBinWidth)
+            %JEROMIAHJPSTH Compute JPSTH using previous methods
+            xCounts = spikeCounts(SpikeUtils.cellArray2mat(alignedSpikesX),timeWin,binWidth);
+            yCounts = spikeCounts(SpikeUtils.cellArray2mat(alignedSpikesY),timeWin,binWidth);
+            outArg = jpsth(xCounts,yCounts,coincidenceBinWidth);
         end
         
         function outArg = alignSpikeTimes(spikes, alignTime, varargin)
-            %ALIGNSPIKETIMES Returns aligned spiketime cell array
+            %ALIGNSPIKETIMES Returns aligned spiketime cell array.  If a
+            %timeWindow [-t t] is given, trims the output to the time
+            %window given
             trim = false;
             nTrials = size(spikes,1);
             if numel(varargin)>0
@@ -24,18 +71,40 @@ classdef SpikeUtils
             elseif numel(alignTime) ~= nTrials
             end
             if ~iscell(spikes)
-                if any(isnan(spikes))
-                    % assume NaN filled spiketime matrix
-                    outArg = arrayfun(@(x) spikes(x,~isnan(spikes(x,:)))-alignTime(x), (1:nTrials)','UniformOutput',false);
-                else
-                    % assume 0 filled spiketime matrix
-                    outArg = arrayfun(@(x) spikes(x,spikes(x,:)>0)-alignTime(x), (1:nTrials)','UniformOutput',false);
-                end
-            else
-                outArg = arrayfun(@(x) spikes{x}-alignTime(x), (1:nTrials)','UniformOutput',false);
+                spikes = SpikeUtils.mat2CellArray(spikes);
             end
+            outArg = arrayfun(@(x) spikes{x}-alignTime(x), (1:nTrials)','UniformOutput',false); 
             if trim
-               outArg = cellfun(@(x) x(x>=timeWin(0) & x<=timeWin(1)), outArg,'UniformOutput',false);
+               outArg = cellfun(@(x) x(x>=timeWin(1) & x<=timeWin(2)), outArg,'UniformOutput',false);
+            end
+        end
+        
+        function outArg = cellArray2mat(spikes)
+            %CELLARRAY2MAT Convert cellarray of timestamps to nan filled
+            %matrix
+            if ~iscell(spikes)
+                error('Input must be a cell array of spike times\n');
+            end
+            maxSpks = max(cellfun(@numel,spikes));
+            outArg = cell2mat(cellfun(@(x) [x nan(1,maxSpks-numel(x))],spikes,'UniformOutput',false));
+        end
+ 
+        function outArg = mat2CellArray(spikes)
+            %MAT2CELLARRAY Convert matrix of timestamps (0 or NaN filled)
+            %filled to cell array of timestamps
+            if iscell(spikes)
+                error('Input must be a matrix of spike times\n');
+            end
+            nTrials = size(spikes,1);
+            if any(isnan(spikes))
+                % assume NaN filled spiketime matrix
+                outArg = arrayfun(@(x) spikes(x,~isnan(spikes(x,:))), (1:nTrials)','UniformOutput',false);
+            else
+                if min(min(spikes)) < 0
+                    error('Unable to convert to cellArray for negative spiketimes. The spike times seem to be aligned on some event.\')
+                end
+                % assume 0 filled spiketime matrix
+                outArg = arrayfun(@(x) spikes(x,spikes(x,:)>0), (1:nTrials)','UniformOutput',false);
             end
         end
                 
@@ -54,7 +123,7 @@ classdef SpikeUtils
             outArg.rasterBins = min(timeWin):max(timeWin);
         end
         
-        function outputArg = psth(cellSpikeTimes,binWidth,timeWin,fx)
+        function outArg = psth(cellSpikeTimes,binWidth,timeWin)
             %PSTH Compute PSTH, PSTH-SD, PSTH-VAR for given spike times.
             %   cellSpikeTimes : Cell array of doubles (aligned spikeTimes).  The
             %      cell array must be {nTrials x 1}. Each clement in the cell
@@ -63,15 +132,16 @@ classdef SpikeUtils
             %   timeWin: [minTime maxTime] for PSTH
             %   fx:  Built-in function-handle to be used for PSTH. Valid
             %        args are: @nanmean, @nanstd, @nanvar
+            outArg.psthBins = min(timeWin):binWidth:max(timeWin);
             binEdges = min(timeWin)-binWidth/2:binWidth:max(timeWin)+binWidth/2;
-            rast = cell2mat(cellfun(@(x) histcounts(x,'BinEdges',binEdges),...
+            outArg.spikeCounts = cell2mat(cellfun(@(x) histcounts(x,'BinEdges',binEdges),...
                 cellSpikeTimes,'UniformOutput',false));
-            outputArg.psth = fx(rast)*1000./binWidth;
-            outputArg.psthBins = min(timeWin):binWidth:max(timeWin);
-            
+            outArg.psth = mean(outArg.spikeCounts)./binWidth;
+            outArg.psthStd = std(outArg.spikeCounts)./binWidth;
+            outArg.psthVar = var(outArg.spikeCounts)./binWidth;
         end
         
-        function outputArg = jpsthXcorrHist(jpsth,lagBins)
+        function outArg = jpsthXcorrHist(jpsth,lagBins)
             %JPSTHXCORRHIST Summary of this method goes here
             %   Detailed explanation goes here
             % Function handle for Cross correlation histogram
@@ -79,16 +149,16 @@ classdef SpikeUtils
             % fx_xcorrh = @(jpsth,lagBins)...
             %     [-abs(lagBins):abs(lagBins);arrayfun(@(x) mean(diag(jpsth,x)),...
             %     -abs(lagBins):abs(lagBins))]';
-            outputArg = [-abs(lagBins):abs(lagBins);arrayfun(@(x) mean(diag(jpsth,x)),...
+            outArg = [-abs(lagBins):abs(lagBins);arrayfun(@(x) mean(diag(jpsth,x)),...
                 -abs(lagBins):abs(lagBins))]';
         end
         
-        function outputArg = jpsthCoincidenceHist(jpsth, lagBins)
+        function outArg = jpsthCoincidenceHist(jpsth, lagBins)
             %JPSTHCOINCIDENCEHIST Summary of this method goes here
             %   Detailed explanation goes here
             % Function handle for psth
             %fx_coinh = @getCoincidence;
-            outputArg = SpikeUtils.getCoincidence_(jpsth,lagBins);
+            outArg = SpikeUtils.getCoincidence_(jpsth,lagBins);
         end
         
         function outputArg = getPsthFuns()
@@ -97,14 +167,14 @@ classdef SpikeUtils
                 };
         end
         
-        function outputArg = getJpsthFuns()
-            outputArg = { @SpikeUtils.jpsthXcorrHist % cross-corr hist fx
+        function outArg = getJpsthFuns()
+            outArg = { @SpikeUtils.jpsthXcorrHist % cross-corr hist fx
                 @SpikeUtils.jpsthCoincidenceHist % coincidence hist fx
                 };
         end
         
-        function outputArg = pspKernel()
-            outputArg = SpikeUtils.getPspKernel_();
+        function outArg = pspKernel()
+            outArg = SpikeUtils.getPspKernel_();
         end
         
         
